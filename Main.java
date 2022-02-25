ac8e60c1cec1242516fb6e7a96056946582a6604
VitDostal <vitek99@atlas.cz>
2022-02-25 11:17:20 +0100

    test

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ package mvcexample;
 public class Main {
    
     public static void main(String args[]) {
-     
+    	// Task4
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model, "Controller 1",40,40); // Create first controller
         Controller2 c2 = new Controller2(model, "Controller 2",40,200); // Create second controller
